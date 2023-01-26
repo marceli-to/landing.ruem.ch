0d8a4f7f3fa448f8744ca77f79166662dd51be8a
Marcel Stadelmann <m@marceli.to>
2023-01-26 15:41:59 +0100

    wip

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>{{ env('APP_NAME') }}</title>
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -11,6 +12,5 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<title>{{ env('APP_NAME') }}</title>
 @vite('resources/js/landing.js')
 </head>
