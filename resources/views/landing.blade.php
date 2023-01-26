dd32a13094d2455df39eeb365fdf37de1a8526a9
Marcel Stadelmann <m@marceli.to>
2023-01-26 16:14:13 +0100

    wip

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,13 +1,13 @@
 @include('partials.head')
 <body class="antialised bg-black relative h-full">
-  <div class="font-sans font-bold text-white text-2xl lg:text-3xl leading-6 md:leading-7 p-4 pt-2 sm:p-4 md:p-8 lg:p-10 absolute top-[60%] sm:top-0 left-0 z-20">
+  <video playsinline autoplay muted loop class="w-full h-auto sm:h-full sm:object-cover">
+    <source src="/video/ruem-low.mp4" type="video/mp4">
+  </video>
+  <div class="font-sans font-bold text-white text-2xl lg:text-3xl leading-8 md:leading-7 p-4 md:p-8 lg:p-10 sm:absolute sm:top-0 sm:left-0 z-20">
     Jenny Schäubli<br>
     dipl. Innenarchitektin<br>
     <a href="tel:0796057922" class="text-white no-decoration hover:underline underline-offset-4 decoration-2">079 605 79 22</a><br>
     <a href="mailto:mail@ruem.ch" class="text-white no-decoration hover:underline underline-offset-4 decoration-2">mail@ruem.ch</a>
   </div>
-  <video playsinline autoplay muted loop class="w-full h-auto sm:h-full sm:object-cover">
-    <source src="/video/ruem-low.mp4" type="video/mp4">
-  </video>
 </body>
 </html>
