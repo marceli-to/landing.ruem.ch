731914308462f75208ddfc31acf41db0b0f3c990
Marcel Stadelmann <m@marceli.to>
2023-01-26 16:09:44 +0100

    wip

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,6 +1,6 @@
 @include('partials.head')
 <body class="antialised bg-black relative h-full">
-  <div class="font-sans font-bold text-white text-lg md:text-2xl lg:text-3xl leading-6 md:leading-7 p-4 pt-0 sm:p-4 md:p-8 lg:p-10 absolute top-[60%] sm:top-0 left-0 z-20">
+  <div class="font-sans font-bold text-white text-lg md:text-2xl lg:text-3xl leading-6 md:leading-7 p-4 pt-2 sm:p-4 md:p-8 lg:p-10 absolute top-[60%] sm:top-0 left-0 z-20">
     Jenny Schäubli<br>
     dipl. Innenarchitektin<br>
     <a href="tel:0796057922" class="text-white no-decoration hover:underline underline-offset-4 decoration-2">079 605 79 22</a><br>
