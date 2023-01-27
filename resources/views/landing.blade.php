bd31fc00749e0c2717d47dab3c96d329a7f1b1bb
Marcel Stadelmann <m@marceli.to>
2023-01-27 14:12:39 +0100

    wip

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
   <video playsinline autoplay muted loop class="w-full h-auto sm:h-full sm:object-cover">
     <source src="/video/ruem-low.mp4" type="video/mp4">
   </video>
-  <div class="font-sans font-bold text-white text-2xl lg:text-3xl leading-8 md:leading-7 p-4 md:p-8 lg:p-10 sm:absolute sm:top-0 sm:left-0 z-20">
+  <div class="font-sans font-bold text-white text-xl leading-7 md:leading-6 p-4 sm:absolute sm:top-0 sm:left-0 z-20">
     Jenny Schäubli<br>
     dipl. Innenarchitektin<br>
     <a href="tel:0796057922" class="text-white no-decoration hover:underline underline-offset-4 decoration-2">079 605 79 22</a><br>
