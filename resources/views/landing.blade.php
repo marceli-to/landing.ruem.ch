3aa7ccfdd9bc5de1f9c993d1812b67e0768f5a91
Marcel Stadelmann <m@marceli.to>
2023-01-31 10:28:32 +0100

    wip

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,6 +1,6 @@
 @include('partials.head')
 <body class="antialised relative h-full flex flex-col justify-between">
-  <div class="font-sans font-bold text-white text-base sm:text-xl leading-6 sm:leading-6 p-4 pb-6 sm:pb-4 sm:absolute sm:top-0 sm:left-0 z-20 flex-1 bg-black sm:bg-transparent relative after:content-[''] after:block sm:after:hidden after:w-full after:h-[100%] after:absolute after:left-0 after:t-[100%] after:translate-y-6 after:z-50 after:bg-gradient-to-b from-black">
+  <div class="font-sans font-bold text-white text-base sm:text-xl leading-6 sm:leading-6 p-4 pb-16 sm:pb-4 sm:absolute sm:top-0 sm:left-0 z-20 flex-1 bg-black sm:bg-transparent relative after:content-[''] after:block sm:after:hidden after:w-full after:h-[100%] after:absolute after:left-0 after:t-[100%] after:translate-y-16 after:z-50 after:bg-gradient-to-b from-black">
     Jenny Schäubli<br>
     dipl. Innenarchitektin<br>
     <a href="tel:0796057922" class="text-white no-decoration hover:underline underline-offset-4 decoration-2">079 605 79 22</a><br>
