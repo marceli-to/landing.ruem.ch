003471519229726eacf74c97b9471ac6a4b10c53
Marcel Stadelmann <m@marceli.to>
2023-01-31 10:13:22 +0100

    wip

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,13 +1,13 @@
 @include('partials.head')
-<body class="antialised bg-black relative h-full">
-  <video playsinline autoplay muted loop class="w-full h-auto sm:h-full sm:object-cover">
-    <source src="/video/ruem-low.mp4" type="video/mp4">
-  </video>
-  <div class="font-sans font-bold text-white text-base lg:text-xl leading-6 lg:leading-6 p-3 lg:p-4 sm:absolute sm:top-0 sm:left-0 z-20">
+<body class="antialised relative h-full flex flex-col justify-between">
+  <div class="font-sans font-bold text-white text-base sm:text-xl leading-6 sm:leading-6 p-4 pb-6 sm:pb-4 sm:absolute sm:top-0 sm:left-0 z-20 flex-1 bg-black sm:bg-transparent relative after:content-[''] after:block sm:after:hidden after:w-full after:h-[100%] after:absolute after:left-0 after:t-[100%] after:translate-y-6 after:z-50 after:bg-gradient-to-b from-black">
     Jenny Schäubli<br>
     dipl. Innenarchitektin<br>
     <a href="tel:0796057922" class="text-white no-decoration hover:underline underline-offset-4 decoration-2">079 605 79 22</a><br>
     <a href="mailto:mail@ruem.ch" class="text-white no-decoration hover:underline underline-offset-4 decoration-2">mail@ruem.ch</a>
   </div>
+  <video playsinline autoplay muted loop class="w-full h-auto h-full object-cover">
+    <source src="/video/ruem-low.mp4" type="video/mp4">
+  </video>
 </body>
 </html>
